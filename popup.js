6156876fe01bbed44b11a69cc869dabc8c27cba7
Oluwapelumi <pelumifafowora@gmail.com>
2020-02-26 14:24:05 -0600

    attempted to parse and iterate through the xml but it didn't work. Will figure out how to fix / proceed later

--- popup.js
+++ popup.js
@@ -1,6 +1,11 @@
 
 
+
+
 document.addEventListener('DOMContentLoaded', function() {
+        var x, i, xmlDoc;
+        var txt = "";
+        var orig_txt;
         var checkPageButton = document.getElementById('xmlButton');
         checkPageButton.addEventListener('click', function() {
                 console.log("hello");
@@ -9,13 +14,20 @@ document.addEventListener('DOMContentLoaded', function() {
                 xhttp.onreadystatechange = function() {
                         
                         if (this.readyState == 4 && this.status == 200) {
-                        document.getElementById("demo").innerHTML =
-                        this.responseText;
+                        //document.getElementById("demo").innerHTML = this.responseText;
+                        orig_txt = this.responseText;
                   }
                 };
                 //xhttp.open("GET", "https://cors-anywhere.herokuapp.com/http://riceowls.com/services/calendar.ashx?type=rss", true);
-                xhttp.open("GET", "https://cors-anywhere.herokuapp.com/http://riceowls.com/services/scores.aspx", true);
-                xhttp.send();
+                xhttp.open("GET", "https://cors-anywhere.herokuapp.com/http://riceowls.com/services/scores.aspx", true); //this is where the request is actually occurring
+                xhttp.send(); // is this sending the request out?
+		parser = new DOMParser();
+		xmlDoc = parser.parseFromString(orig_txt,"text/xml")//First parameter is the xml text and the second parameter is the content type of the 
+                x = xmlDoc.documentElement.childNodes;
+                for (i = 0; i < x.length; i++) {
+                        txt += x[i].nodeName + ": " + x[i].childNodes[0].nodeValue + "<br>"
+                }
+                document.getElementById("demo").innerHTML = txt;
         }, false);
       }, false);
 
